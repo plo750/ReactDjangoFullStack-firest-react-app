efe16b9ba7ad0b79b421a00bf252464e8bf0b91b
Pedro Lopez Perez <pedro.lopez-perez@etat.ge.ch>
2021-11-28 23:07:22 +0000

    S04-47 State

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -4,12 +4,20 @@ import React, { Component } from 'react';
 // React class as component
 class Footer extends Component { // props are inherited from Component
 
+  //
+  state = {
+    name: 'Pedro',
+    age: '54'
+  }
+
   createAlert() {
     alert('Hello, you clicked me from child');
   }
 
-  changed() {
-    console.log('changed');
+  // Arrow function
+  changed = evt => {
+    this.setState({name: evt.target.value})
+    console.log('changed', this.state.name);
   }
 
   // Override render function
@@ -20,7 +28,8 @@ class Footer extends Component { // props are inherited from Component
         <h2 onClick={this.props.myAlert}>
           {this.props.trademark}
         </h2>
-        <input onChange={this.changed}type="text" />
+        <input value={this.state.name}
+          onChange={this.changed} type="text" />
       </React.Fragment>
     )
   }
