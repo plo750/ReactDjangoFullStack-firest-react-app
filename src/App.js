c338be594ac33f1cd1deedacd894780180a6e773
Pedro Lopez <plp750@gmail.com>
2021-12-05 17:22:02 +0100

    S04-53 Styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './Components/header' // option 2
 import Footer from './Components/footer' // option 2
 
-
+import styled from 'styled-components'
 
 function createAlert() {
   alert('Hello, you clicked me from parent');
@@ -19,11 +19,22 @@ function ShowMessage(props) {
 }
 */
 
+const pStyle = {
+  fontSize: '2em', // css font-size ==> fontSize
+  color: 'red'
+}
+
+const Paragraph = styled.p`
+  font-size: 3em;
+  color: green;
+`;
+
 function App() {
     return (
       <div className="App">
         <Header info="This is my message" />
-        <p>main content</p>
+        <p style={pStyle}>main content</p>
+        <Paragraph>New styled</Paragraph>
         <Footer trademark="Page made by Pedro" myAlert={createAlert}/>
         { /* <ShowMessage toShow={false}/> */  }
       </div>
